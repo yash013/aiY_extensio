85a9af3003bdb6665f399d4f5e69289aa1e62523
yash013 <yashsuthar013@gmail.com>
2023-08-05 08:35:55 +0530

    command name changed

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 async function handleKeyup(event) {
   if (event.key === "Enter") {
-    const tweetBox = event.target;
-    const tweetContent = event.target.textContent.trim();
-    console.log("Element data:", event.target, tweetContent);
-    const regex = /^\/y (.+)$/;
-    const match = tweetContent.match(regex);
+    const textBox = event.target;
+    const textBoxContent = event.target.textContent.trim();
+    console.log("Element data:", event.target, textBoxContent);
+    const regex = /^\/gni (.+)$/;
+    const match = textBoxContent.match(regex);
+
     if (match) {
       event.preventDefault();
       const prompt = match[1];
@@ -30,40 +31,27 @@ async function handleKeyup(event) {
             }),
           }
         );
-        // const apiKey = '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';
-        // const response = await fetch('https://api.openai.com/v1/engines/text-davinci-003/completions', {
-        //   method: 'POST',
-        //   headers: {
-        //     'Content-Type': 'application/json',
-        //     'Authorization': `Bearer ${apiKey}`
-        //   },
-        //   body: JSON.stringify({
-        //     prompt: prompt,
-        //     max_tokens: 2048,
-        //     temperature: 0.3
-        //   })
-        // });
 
         if (response.ok) {
           const data = await response.json();
           console.log({ data });
           const outputText = data.choices[0].message.content;
 
-          const newTweetContent = `/y ${prompt}\n${outputText}`;
+          const newTextContent = `/y ${prompt}\n${outputText}`;
           // Check if the tweet box has an input element
-          const inputElement = tweetBox.querySelector("input");
+          const inputElement = textBox.querySelector("input");
           if (inputElement) {
-            inputElement.value = newTweetContent;
+            inputElement.value = newTextContent;
           } else {
             // If no input element found, fallback to setting the textContent
-            tweetBox.textContent = newTweetContent;
+            textBox.textContent = newTextContent;
           }
 
           // Trigger the 'input' event to update the UI
-          tweetBox.dispatchEvent(new Event("input", { bubbles: true }));
+          textBox.dispatchEvent(new Event("input", { bubbles: true }));
 
-          // tweetBox.innerText = newTweetContent;
-          tweetBox.focus();
+          // textBox.innerText = newTextContent;
+          textBox.focus();
         } else {
           console.error(
             "Failed to retrieve OpenAI response:",
